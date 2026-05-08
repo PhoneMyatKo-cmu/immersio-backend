8c6c4902e190bd092a1f117b77bcfbecca1b4eb9
Phone <hponemyut@gmail.com>
2026-05-09 03:15:31 +0700

    Implement sentence reconstruction using rule-based approach, fetch sub-fragment level tokens using yt-dlp for more robust reconstruction than raw caption fragments returned  by youtube

--- api/v1/endpoints/user_add_video.py
+++ api/v1/endpoints/user_add_video.py
@@ -7,6 +7,9 @@ from db.base import get_db
 from services.caption_services import save_tokenized_captions
 from services.vocab_services import save_vocabularies
 from utils.captions_helpers import fetch_raw_captions,tokenize_captions,process_captions
+from utils.sentence_reconstruction import reconstruct_as_dicts
+from services.context_sentence_services import save_context_sentence
+
 router=APIRouter(prefix="/add-video")
 
 class VideoUrl(BaseModel):
@@ -15,14 +18,14 @@ class VideoUrl(BaseModel):
 @router.post("/")
 async def add_video(data:VideoUrl,
                     db:AsyncSession=Depends(get_db)):
+    
     validation_result=await validate_video(data.youtube_url)
     if not validation_result["valid"]:
         return {
             "status":"fail",
-            "error":validation_result["error"]
-            
-            
+            "error":validation_result["error"]  
         }
+        
     video_id=validation_result["video_id"]
     is_video_existing=await check_video_exists(video_id,db)
     if is_video_existing:
@@ -36,13 +39,13 @@ async def add_video(data:VideoUrl,
         suitability=validation_result["suitablity"],
         db=db
     )
-    saved_video_db_id=get_video_by_url(video_id,db).id
     
+    saved_video_db_id=get_video_by_url(video_id,db).id
     raw_captions=fetch_raw_captions(video_id)
     processed_captions=process_captions(raw_captions)
     caption_len=0
     flattened_token=[t["surface"] for caption in processed_captions for t in caption["tokens"]]
-    print(flattened_token[:5])
+    # print(flattened_token[:5])
     try:
         caption_len=save_tokenized_captions(processed_captions,saved_video_db_id,db)
     except Exception:
@@ -52,12 +55,16 @@ async def add_video(data:VideoUrl,
     save_vocabularies(flattened_token,db)
     
     
-       
+    # Sentence saving
+    context_sentences=reconstruct_as_dicts(video_id)
+    saved_sentences=save_context_sentence(context_sentences,saved_video_db_id,db)  
     return {
         "status":"success",
         "error":None,
         "title":video["title"],
-        "caption_len":caption_len
+        "video_db_id":saved_video_db_id,
+        "caption_len":caption_len,
+        "num_of_sentence":saved_sentences["number_of_sentences"]
     }
 
     
