11e2a8708ce77eda2502ca42713b9b612c01e780
Phone <hponemyut@gmail.com>
2026-05-18 23:31:32 +0700

    Modify video related file adding error handling.

--- api/v1/endpoints/TempVideo.py
+++ api/v1/endpoints/TempVideo.py
@@ -1,20 +1,17 @@
-from fastapi import APIRouter,HTTPException,Depends
-from pydantic import BaseModel, ConfigDict
-from db.base import get_db
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from services.video_services import get_video_by_id
-router=APIRouter(prefix="/video")
 
-class VideoRespone(BaseModel):
-    youtube_video_id:str
-    title:str
-    thumbnail_url:str
-    channel_name:str
-    duration_seconds:int
-    
-    model_config=ConfigDict(from_attributes=True)
+from db.base import get_db
+from schemas.video import VideoRespone
+from services.video_services import get_video_by_id
+
+router = APIRouter(prefix="/video")
+
 
 @router.get("/get-by-id")
-def get_video(id:int,db:Session=Depends(get_db))->VideoRespone:
-    video=get_video_by_id(id,db)
-    return video
+def get_video(id: int, db: Session = Depends(get_db)) -> VideoRespone:
+
+    video = get_video_by_id(id, db)
+    if video is not None:
+        return video
+    raise HTTPException(status_code=404, detail="Video not found.")
