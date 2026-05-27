38ac9067a9780b9e13ef046af4598c35ac85f7a8
WaiYanMoeLwin <waiyanmoelwin1710@gmail.com>
2026-05-27 17:38:47 +0700

    added pagination

--- api/v1/endpoints/home_feed.py
+++ api/v1/endpoints/home_feed.py
@@ -8,11 +8,20 @@ router = APIRouter(prefix="/feed")
 
 @router.get("/")
 def get_home_feed(db: Session = Depends(get_db),
-                  search: str = None):
-    videos = get_videos(db, search=search.lower() if search else None)
-    return videos
+                search: str = None,
+                page: int = 1,
+                page_size: int = 6):
+    videos, total_videos = get_videos(db, search=search.lower() if search else None, page=page, page_size=page_size)
+    total_pages = (total_videos + page_size - 1) // page_size
+    print(f"Total videos: {total_videos}, Total pages: {total_pages}")
+    return videos, total_pages
 
 @router.get("/difficulty/{difficulty_level}")
-def get_videos_by_difficulty(difficulty_level: DifficultyLevel, db: Session = Depends(get_db)):
-    videos = get_videos_by_difficulty_level(difficulty_level, db)
-    return videos
+def get_videos_by_difficulty(db: Session = Depends(get_db),
+                            difficulty_level: DifficultyLevel = DifficultyLevel.beginner,
+                            search: str = None,
+                            page: int = 1,
+                            page_size: int = 6):
+    videos, total_videos = get_videos_by_difficulty_level(search, difficulty_level, db, page, page_size)
+    total_pages = (total_videos + page_size - 1) // page_size
+    return videos, total_pages
--- services/video_services.py
+++ services/video_services.py
@@ -1,4 +1,4 @@
-from sqlalchemy import text
+from sqlalchemy import func, text
 from models.video import DifficultyLevel, Video
 from sqlalchemy.orm import Session
 from sqlalchemy import select
@@ -53,17 +53,22 @@ def get_video_by_youtube_video_id(youtube_video_id:str,db:Session):
     row = db.execute(stmt).scalars().first()
     return row
 
-def get_videos_by_difficulty_level(difficulty_level: DifficultyLevel,db:Session):
+def get_videos_by_difficulty_level(difficulty_level: DifficultyLevel,db:Session, search: str, page: int, page_size: int):
     stmt = select(Video).where(
     Video.difficulty_level == difficulty_level
     ).order_by(
         Video.created_at.desc()
-    ).limit(10)
+    )
 
-    rows = db.execute(stmt).scalars().all()
-    return rows
+    if search:
+        stmt = stmt.where(Video.title.ilike(f"%{search}%"))
 
-def get_videos(db:Session, search: str = None):
+    rows = db.execute(stmt.limit(page_size).offset((page - 1) * page_size)).scalars().all()
+    
+    total_videos = get_total_video_count(db, stmt)
+    return rows, total_videos
+
+def get_videos(db:Session, search: str = None, page: int = 1, page_size: int = 6):
     stmt = select(Video).order_by(
         Video.created_at.desc()
     )
@@ -71,5 +76,10 @@ def get_videos(db:Session, search: str = None):
     if search:
         stmt = stmt.where(Video.title.ilike(f"%{search}%"))
 
-    rows = db.execute(stmt).scalars().all()
-    return rows
+    rows = db.execute(stmt.limit(page_size).offset((page - 1) * page_size)).scalars().all()
+    total_videos = get_total_video_count(db, stmt)
+    return rows, total_videos
+
+def get_total_video_count(db:Session, stmt:select):
+    total_videos = db.scalar(select(func.count()).select_from(stmt.subquery()))
+    return total_videos
